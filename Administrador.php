0a10a10086d37666aae3b16ca030b0699c5c3bfa
Xavier Cobos <xcobos@espol.edu.ec>
2023-08-20 21:19:46 -0500

    Saber si consta} el producto en la base

--- Administrador.php
+++ Administrador.php
@@ -2,7 +2,7 @@
 class Administrador {
     // Función para verificar si el usuario es administrador
     public function is_admin() {
-        //Dependera de usuario...
+        // Depende de usuario
         return true;
     }
 
@@ -12,24 +12,37 @@ class Administrador {
             // Código para editar un producto en un archivo .csv
             $file = fopen('products.csv', 'r');
             $data = array();
+            $product_found = false;
             while(($row = fgetcsv($file)) !== FALSE) {
                 if($row[0] == $product_id) {
                     $row[1] = $name;
                     $row[2] = $price;
+                    $product_found = true;
                 }
                 $data[] = $row;
             }
             fclose($file);
-            $file = fopen('products.csv', 'w');
-            foreach($data as $row) {
-                fputcsv($file, $row);
+            if($product_found) {
+                $file = fopen('products.csv', 'w');
+                foreach($data as $row) {
+                    fputcsv($file, $row);
+                }
+                fclose($file);
+                echo "Producto actualizado con éxito";
             }
-            fclose($file);
-            echo "Producto actualizado con éxito";
+            else {
+                echo "No se encontró el producto especificado";
+            }
         }
         else {
             echo "No tienes permiso para editar productos";
         }
     }
 }
+
+
+$administrador = new Administrador();
+$administrador->edit_product(1, 'Peras', 9.99);
+
+
 ?>
