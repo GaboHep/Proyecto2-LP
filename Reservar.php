15d655a4b10514adae742d7f8622848f7f72a0c3
Xavier Cobos <xcobos@espol.edu.ec>
2023-08-20 21:30:26 -0500

    Ahora reservas ve si existe ese producto

--- Reservar.php
+++ Reservar.php
@@ -2,11 +2,34 @@
 class Reservar {
     // Función para realizar una reserva de un producto
     public function reserve_product($product_id, $quantity) {
-        // Código para agregar una reserva a un archivo .csv
-        $file = fopen('reservations.csv', 'a');
-        fputcsv($file, array($product_id, $quantity));
+        // Verificar si el producto existe en el archivo products.csv
+        $product_found = false;
+        $product_price = 0;
+        $file = fopen('products.csv', 'r');
+        while(($row = fgetcsv($file)) !== FALSE) {
+            if($row[0] == $product_id) {
+                $product_found = true;
+                $product_price = $row[2];
+                break;
+            }
+        }
         fclose($file);
-        echo "Reserva realizada con éxito";
+        
+        if($product_found) {
+            // Si el producto existe, calcular el precio total y agregar la reserva al archivo reservations.csv
+            $total_price = $quantity * $product_price;
+            $file = fopen('reservations.csv', 'a');
+            fputcsv($file, array($product_id, $quantity, $total_price));
+            fclose($file);
+            echo "Reserva realizada con éxito";
+        }
+        else {
+            echo "No se encontró el producto especificado";
+        }
     }
 }
+
+$reservar = new Reservar();
+$reservar->reserve_product(1, 2);
+
 ?>
